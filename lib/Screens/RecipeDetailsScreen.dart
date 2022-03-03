a8dd8705e1c969a316a6672c1fc8dc7e0652cde7
mahmoud-naser <mahmodammarnaser1974@gmail.com>
2022-03-03 10:59:03 +0200

    First version

--- lib/Screens/RecipeDetailsScreen.dart
+++ lib/Screens/RecipeDetailsScreen.dart
@@ -276,7 +276,7 @@ class _DetailsScreenState extends State<DetailsScreen>
                   onPressed: () {
                     Provider.of<settings>(context, listen: false)
                         .changeLanguage(Language.en);
-                    Data.storeLanguage("Enggit ");
+                    Data.storeLanguage("Esdfvcdscdscdsit ");
                     S.load(Locale("en", "US"));
                   })
             ],
